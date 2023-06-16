2faf6a99ef4f24e4502828aaac322d85032db2f8
Raphael Iyin <RaphaelLyin@MacBook-Pro.lan>
2023-06-16 14:16:44 -0400

    initial commit

---
+++ HiitFit/HiitFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiitFitApp.swift
+//  HiitFit
+//
+//  Created by Raphael Iyin on 6/16/23.
+//
+
+import SwiftUI
+
+@main
+struct HiitFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
